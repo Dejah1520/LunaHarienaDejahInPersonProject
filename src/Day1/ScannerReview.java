95119cb54a91581122316848c513801d2a3d5b47
dejahmartin <dejah1520@gmail.com>
2021-10-28 13:51:41 -0500

    Day One Scanner

--- src/Day1/ScannerReview.java
+++ src/Day1/ScannerReview.java
@@ -2,9 +2,13 @@ package Day1;
 
 import java.util.Scanner;
 
-public class ScannerOne {
+public class ScannerReview {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter your name: ");
+        String name = scan.nextLine();
+
+
 
 
 
