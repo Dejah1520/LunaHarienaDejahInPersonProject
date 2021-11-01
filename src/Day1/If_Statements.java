7980e007a0270b3dbe7d0ac9ae9b9543a61a835f
dejahmartin <dejah1520@gmail.com>
2021-11-01 10:00:34 -0500

    if statements practice and review

--- src/Day1/If_Statements.java
+++ src/Day1/If_Statements.java
@@ -3,6 +3,60 @@ package Day1;
 public class If_Statements {
 
 
+   /* 1.Write a Java program to get a number from the user and print whether it is positive or negative.
+
+    Test Data
+    Input number: 35
+    Expected Output :
+    Number is positive
+
+
+    2. Write a Java program to solve quadratic equations (use if, else if and else).
+    Test Data
+    Input a: 1
+    Input b: 5
+    Input c: 1
+    Expected Output :
+    The roots are -0.20871215252208009 and -4.7912878474779195
+
+
+    3. Take three numbers from the user and print the greatest number.
+
+    Test Data
+    Input the 1st number: 25
+    Input the 2nd number: 78
+    Input the 3rd number: 87
+    Expected Output :
+    The greatest: 87
+
+
+    4. Write a Java program that reads a floating-point number and prints "zero" if the number is zero. Otherwise, print "positive" or "negative". Add "small" if the absolute value of the number is less than 1, or "large" if it exceeds 1,000,000. Go to the editor
+
+       Test Data
+       Input a number: 25
+       Expected Output :
+       Input value: 25
+       Positive number
+
+
+       5. Write a Java program that keeps a number from the user and generates an integer between 1 and 7 and displays the name of the weekday.
+
+        Test Data
+        Input number: 3
+        Expected Output :
+        Wednesday
+
+    */
+
+
+
+
+
+
+
+
+
+
 
 
 
